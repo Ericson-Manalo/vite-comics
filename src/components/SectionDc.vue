d9df475cc9bb842f82520746b6214dba1da92e6a
Ericson-Manalo <ericmana98@gmail.com>
2023-06-15 20:10:12 +0200

    created a new component called Section

---
+++ src/components/SectionDc.vue
@@ -0,0 +1,31 @@
+<script>
+
+export default{
+    name: 'AppSection',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<template>
+    <section>
+
+    </section>
+</template>
+
+<style lang="scss">
+
+@use '../styles/partials/mixins' as *;
+@use '../styles/partials/variables' as *;
+
+section{
+    display: flex;
+    height: 160px;
+    background-color:$lightblue;
+}
+
+
+</style>
