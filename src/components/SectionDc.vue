9751f98ee6a6bf7a90ee74bbda79ebff6555678e
Ericson-Manalo <ericmana98@gmail.com>
2023-06-15 20:24:57 +0200

    added the logo's and changed name on each icon

--- src/components/SectionDc.vue
+++ src/components/SectionDc.vue
@@ -12,6 +12,38 @@ export default{
 
 <template>
     <section>
+        <div class="main-section">
+            <div class="item-container flex">
+                <img src="../assets/images/img/buy-comics-digital-comics.png" alt="Digital Comics">
+                <p>
+                    DIGITAL COMICS 
+                </p>
+            </div>
+            <div class="item-container flex">
+                <img src="../assets/images/img/buy-comics-merchandise.png" alt="DC Merchandise">
+                <p>
+                    DIGITAL COMICS 
+                </p>
+            </div>
+            <div class="item-container flex">
+                <img src="../assets/images/img/buy-comics-subscriptions.png" alt="Subscription">
+                <p>
+                    DIGITAL COMICS 
+                </p>
+            </div>
+            <div class="item-container flex">
+                <img src="../assets/images/img/buy-comics-shop-locator.png" alt="Comic Shop Locator">
+                <p>
+                    DIGITAL COMICS 
+                </p>
+            </div>
+            <div class="item-container flex">
+                <img src="../assets/images/img/buy-dc-power-visa.svg" alt="DC Power Visa">
+                <p>
+                    DIGITAL COMICS 
+                </p>
+            </div>
+        </div>
 
     </section>
 </template>
@@ -25,6 +57,28 @@ section{
     display: flex;
     height: 160px;
     background-color:$lightblue;
+    display: flex;
+    @include centering(center, center);
+    color: $whiteText;
+    font-size: .9rem;
+}
+
+div.main-section{
+    width: 1100px;
+    display: flex;
+    justify-content: space-around;
+}
+
+div.item-container img{
+    width: 1100px;
+    width: 42px;
+    height: 50px;
+}
+
+div.item-container p{
+    align-items: center;
+    display: flex;
+    margin-left: 1rem;
 }
 
 
