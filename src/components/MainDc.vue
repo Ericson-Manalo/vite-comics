2b624a9d222312230ea5d83c4e134bce310af08b
Ericson-Manalo <ericmana98@gmail.com>
2023-06-15 16:30:33 +0200

    created component main

--- src/components/MainDc.vue
+++ src/components/MainDc.vue
@@ -1,13 +1,44 @@
 <script>
 
+export default{
+    name: 'Appmain',
+    data(){
+        return{
+
+        }
+    }
+}
+
 </script>
 
 <template>
-    <h1>
-        main
-    </h1>
+    <main>
+        <div class="main-content">
+            <h1>
+            - - Content goes here - -
+            </h1>
+        </div>
+    </main>
 </template>
 
-<style>
+<style lang="scss" scoped>
+@use '../styles/partials/mixins' as *;
+@use '../styles/partials/variables' as *;
+
+main{
+    display: flex;
+    justify-content: center;
+    background-color: $darkBg;
+    padding: 3rem;
+
+    div.main-content{
+        width: 1100px;
+    }
+}
+
+h1{
+    color: $whiteText;
+}
+
 
 </style>
