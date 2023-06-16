0edf7155da272ee7f1f3d035ff01f0c4b2ed34bb
Ericson-Manalo <ericmana98@gmail.com>
2023-06-16 19:25:02 +0200

    created the whole singlecardDc component by giving it the property to make 1 single card

---
+++ src/components/SingleCardDc.vue
@@ -0,0 +1,43 @@
+<template lang="">
+    <article>
+        <div class="card-container">
+            <img :src="cardElement.thumb" alt="DC Cards" class="card">
+            <h3>
+            {{cardElement.series}}
+            </h3>
+        </div>
+    </article>
+    
+
+</template>
+<script>
+export default {
+    name: 'SingleCardDc',
+    props: [
+        'cardElement'
+    ]
+}
+</script>
+<style lang="scss" scoped>
+
+@use '../styles/partials/mixins' as *;
+@use '../styles/partials/variables' as *;
+
+    article{
+        width: calc(100% / 6);
+    }
+
+    img{
+        width: 170px;
+        height: 170px;
+    }
+
+    div.card-container h3{
+        padding-top: .8rem;
+        color: $whiteText;
+        text-transform: uppercase;
+        font-weight: 400;
+        font-size: 1rem;
+        text-align: center;
+    }
+</style>
